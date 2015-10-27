a39ffcfa8c5381c3bfd34875fdd4ca23ca3dfa84
don <don@8ths-MacBook-Pro.local>
2015-10-27 16:48:51 -0700

    updated tests and core code for stories 2 and 1

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,21 +1,57 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
+  (defn create-empty-board []
+	["_" "_" "_" "_" "_" "_" "_" "_" "_"])
+   
+  (defn move 
+    [location player board]
+    (assoc board location player ))
 
-
-(defn create-board 
-  [columns rows]
-   )
-
-(defn move 
-  [location player board]
-  (assoc board (keyword location) player ))
+  
+(defn validmove? [move]
+  	(if (and (<= move 8)(>=  move 0))
+      true 
+      false
+  	))
 
 (defn user-input-move []
-	(println "what is your next move")
-	(read-line))
+    (println "what is your next move")
+    (def input (read-string(read-line)))
+    (if(= true (validmove? input))
+      input
+      (do (println "invalid selection") 
+      	   (user-input-move))))
+  
+  (defn computer-move [board]
+  	(.indexOf board "_"))
 
-(defn display-board [board]
- (println (str (vals board)))
+  (defn display-board [board]
+    (println  board)
+  )
 
-)
+ (defn clear-terminal[]
+   (println "\033[2J"))
+
+(defn game-runner 
+ ([welcome]
+   (println welcome)
+   (def current-board (create-empty-board))
+   (println current-board)
+   	(def current-board (move (user-input-move) "X" current-board)) 
+    (clear-terminal)
+    (display-board current-board)
+   (game-runner current-board "X"))
+ ([board player]
+	(def current-board (move (computer-move board) "O" board))
+    (clear-terminal)
+    (display-board current-board)
+    (def current-board (move (user-input-move) player current-board))
+    (clear-terminal)
+    (display-board current-board)
+    (game-runner current-board "X")))
+
+(defn -main []
+   ; (def move-chart ["0" "1" "2" "3" "4" "5" "6" "7" "8"])
+  (def welcome "welcome to Dons tic tac toe")
+  (game-runner  welcome ))
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -1,33 +1,36 @@
 (ns tic-tac-toe.core-test
   (:require [clojure.test :refer :all]
             [tic-tac-toe.core :refer :all]))
-(def board { :1 "_"
-                   :2 "_"
-                   :3 "_" 
-                   :4 "_" 
-                   :5 "_" 
-                   :6 "_" 
-                   :7 "_" 
-                   :8 "_" 
-                   :9 "_"})
-(def player-one "X")
-(def Player-two "Y")
-
-
-
+(def board ["_" "_" "_" "_" "_" "_" "_" "_" "_"])
 
 (deftest board-move
 	(testing "Create new board based on user input"
-		(is ( =  {:4 "_", :7 "_", :1 "_", :8 "_", :9 "_", :2 "_", :5 "X", :3 "_", :6 "_"} ( move "5" player-one  board )))))
-
+		(is ( =  ["_" "_" "_" "_" "_" "_" "_" "_" "_"] ( create-empty-board )))))
+(deftest valid-move
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 0))))
 
 (deftest get-move
   (testing "gets players move"
-	(is (= "5" (with-in-str "5" (user-input-move))))))
+	(is (= 4 (with-in-str "4"(user-input-move))))))
+
+(deftest view-clear
+	(testing "clears terminal"
+	(is (= (println "\033[2J" ) (clear-terminal)))))
+
+(deftest view-show
+	(testing "dispays the board"
+	( is(= (println board)(display-board board)))))
+
+(deftest computer-next-move 
+	(testing "Computer next move"
+	(is (=  0 (computer-move board)))))
 
 
-(deftest print-board
-	(testing "prints boards current state"
-	(is (= (println "____\n____\n____\n" ) (display-board board)))))
 
+; (deftest game-runner
+; 	(testing "gets user input and updates the board"
+;   (is (= (println ""))
+
+		
 
