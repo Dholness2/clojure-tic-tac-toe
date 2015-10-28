ca03bed1aaa90993cfbf1a4a6fdd249700a5eb12
don <don@8ths-MacBook-Pro.local>
2015-10-28 16:51:27 -0700

    Added row col draw checks and updated board display

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -26,32 +26,61 @@
   (defn computer-move [board]
   	(.indexOf board "_"))
 
-  (defn display-board [board]
-    (println  board)
-  )
+  (defn display-board [board w]
+  	(if(= false (empty? board))
+  	  (let[ row (take w board)]
+        (println  (clojure.string/join row))
+		(display-board(drop w board)w))
+  	    ))
 
- (defn clear-terminal[]
-   (println "\033[2J"))
 
-(defn game-runner 
+  (defn row-check [board width]
+  	(let[ row (vec(take width board)) ]
+      (if (or (= "_" (some #{"_"} row)) (>= (count(distinct row)) 2)) 
+                (row-check (drop width board) width)
+            	(row 0))))
+
+ (defn column-check [board width columns]
+    (let 	
+  	[ col (vec(map (fn [index](get board index)) [(- width columns) (+ (- width columns) width) (+ (- width columns) (* width 2)) ]))]
+    (if (or (= "_" (some #{"_"} col)) (>= (count (distinct col)) 2))
+       (column-check board width (- columns 1))
+       (col 0))))
+
+
+  (defn  draw? [board]
+  	(if-not(= "_" (some #{"_"} board))
+      true
+      false))
+
+
+(defn clear-terminal[]
+    (println "\033[2J"))
+  
+
+  (defn game-runner 
  ([welcome]
    (println welcome)
    (def current-board (create-empty-board))
-   (println current-board)
+   (display-board current-board 3)
    	(def current-board (move (user-input-move) "X" current-board)) 
     (clear-terminal)
-    (display-board current-board)
+    (display-board current-board 3)
    (game-runner current-board "X"))
  ([board player]
 	(def current-board (move (computer-move board) "O" board))
     (clear-terminal)
-    (display-board current-board)
+    (display-board current-board 3 )
     (def current-board (move (user-input-move) player current-board))
     (clear-terminal)
-    (display-board current-board)
+    (display-board current-board 3)
     (game-runner current-board "X")))
 
 (defn -main []
    ; (def move-chart ["0" "1" "2" "3" "4" "5" "6" "7" "8"])
   (def welcome "welcome to Dons tic tac toe")
   (game-runner  welcome ))
+
+
+
+ 
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -6,9 +6,50 @@
 (deftest board-move
 	(testing "Create new board based on user input"
 		(is ( =  ["_" "_" "_" "_" "_" "_" "_" "_" "_"] ( create-empty-board )))))
-(deftest valid-move
+(deftest valid-move-zer0
  (testing "test to mke sure user input is within game move option[0-8]")
  	(is (= true (validmove? 0))))
+
+(deftest valid-move-one
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 1))))
+
+(deftest valid-move-two
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 2))))
+
+(deftest valid-move-three
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 3))))
+
+(deftest valid-move-four
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 4))))
+
+(deftest valid-move-five
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 5))))
+
+(deftest valid-move-six
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 6))))
+
+(deftest valid-move-seven
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 7))))
+
+(deftest valid-move-eight
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= true (validmove? 8))))
+
+
+(deftest invalid-move-less-than-zero
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= false (validmove? -1))))
+
+(deftest invalid-move-greater-than-eight
+ (testing "test to mke sure user input is within game move option[0-8]")
+ 	(is (= false (validmove? 20))))
 
 (deftest get-move
   (testing "gets players move"
@@ -20,11 +61,28 @@
 
 (deftest view-show
 	(testing "dispays the board"
-	( is(= (println board)(display-board board)))))
+	( is(= (println board)(display-board board 3 )))))
 
 (deftest computer-next-move 
 	(testing "Computer next move"
 	(is (=  0 (computer-move board)))))
+
+(deftest board-draw 
+	(testing "game is a draw"
+	(is (=  true (draw? ["x" "y" "y" "x" "x" "Y" "Y" "x" "y" ])))))
+
+(deftest board-row-check 
+	(testing "game is a draw"
+	(is (=  "x" (row-check ["x" "x" "x" "y" "x" "Y" "Y" "x" "y" ] 3 )))))
+
+
+(deftest board-colunm-check 
+	(testing "game is a draw"
+	(is (=  "x" (column-check ["y" "x" "x" "y" "x" "Y" "Y" "x" "y" ] 3 3 )))))
+
+
+
+
 
 
 
