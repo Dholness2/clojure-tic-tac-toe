a7b16340b29991fd2972c742291930b4eff49cf4
don <don@8ths-MacBook-Pro.local>
2015-11-17 10:28:24 -0800

    moved constants to game namespace

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,22 +1,18 @@
 (ns tic-tac-toe.core
   (:gen-class)
   (require [tic-tac-toe.board :refer :all]
+           [tic-tac-toe.game :refer :all]
            [tic-tac-toe.ai :refer :all]
            [tic-tac-toe.human :refer :all]
-           [tic-tac-toe.display :refer :all]
-           [tic-tac-toe.game :refer :all]))
-
-(def human-place-holder "x")
-(def computer-place-holder "o")
-(def board-dimensions 3 )
+           [tic-tac-toe.display :refer :all]))
 
 (defn game-runner [board]
   (display-iteration board)
   (if-not (winner? board)
     (let [user-move (user-input-move board board-dimensions)
-          current-board (move user-move human-place-holder board)]
+          current-board (move user-move player1-marker board)]
       (display-iteration current-board)
-      (if (ai-move current-board) (game-runner (move (ai-move current-board) computer-place-holder current-board))))
+      (if (ai-move current-board) (game-runner (move (ai-move current-board) player2-marker current-board))))
     (print-winner board)))
 
 (defn -main []
