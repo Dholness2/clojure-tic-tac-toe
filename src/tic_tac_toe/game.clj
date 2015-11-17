051391d5e1e53784a5652b9486aa5bc6af996bf9
don <don@8ths-MacBook-Pro.local>
2015-11-17 10:38:18 -0800

    added player markers and board dimensions constasts to name space

--- src/tic_tac_toe/game.clj
+++ src/tic_tac_toe/game.clj
@@ -1,8 +1,13 @@
 (ns tic-tac-toe.game
   (:require [tic-tac-toe.board :refer :all]))
 
+(def player1-marker "x")
+(def player2-marker "o")
+(def board-dimensions 3 )
+
+
 (defn  draw? [board]
-  (if-not(= "_" (some #{"_"} (flatten board)))
+  (if-not(= empty-space (some #{empty-space} (flatten board)))
     true
     false))
 
@@ -10,7 +15,7 @@
   (cond
     (row-check board) (row-check board)
     (column-check board) (column-check board)
-    (diagonal-check board 3) (diagonal-check board 3)
+    (diagonal-check board board-dimensions) (diagonal-check board board-dimensions)
     (draw? board ) "its a draw"))
 
 (defn game-depth [board]
