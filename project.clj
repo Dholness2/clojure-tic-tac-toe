1ea58726b6e0a5677af990f2ce2b292ec8413bae
don <don@8ths-MacBook-Pro.local>
2015-11-25 16:27:39 -0800

    Added an auto-runner for our tests

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"] [net.mikera/core.matrix "0.43.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.43.0"]]
   :main ^:skip-aot tic-tac-toe.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :aliases {"autotest" ["with-profile" "test" "auto" "test"]}
+  :profiles {:test {:plugins [[lein-auto "0.1.2"]]}
+  			 :uberjar {:aot :all}})
