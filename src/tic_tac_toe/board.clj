415b73dfb3c0fed34de12dfc5f8d402989b1ec94
Donovan Holness <dholness@8thlight.com>
2015-12-07 15:30:54 -0800

    Removed a space

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -36,6 +36,6 @@
   (map (fn [location] (get-in board [location location])) locations))
 
 (defn get-diagnoals [board rowsize]
-  (let [diagonal-indexs-top (vec(take rowsize (iterate inc 0)))
- 	      diagonal-indexs-bottom (vec(take rowsize (iterate dec (- rowsize 1))))]
+  (let [diagonal-indexs-top (vec (take rowsize (iterate inc 0)))
+ 	      diagonal-indexs-bottom (vec (take rowsize (iterate dec (- rowsize 1))))]
     [(get-location board diagonal-indexs-top) (get-location (vec (reverse board)) diagonal-indexs-bottom)]))
