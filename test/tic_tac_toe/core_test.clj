84e150b4355748474ec39d968c53aee59e2d9cf7
Donovan Holness <dholness@8thlight.com>
2015-12-11 16:03:46 -0800

    added updated test for game Intializer

--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -1,6 +1,6 @@
 (ns tic-tac-toe.core-test
   (:require [clojure.test :refer :all]
-            [tic-tac-toe.core :refer [game-intializer game-runner create-game marker-one marker-two]]
+            [tic-tac-toe.core :refer [game-intializer game-runner create-game marker-one marker-two games]]
             [tic-tac-toe.board :refer :all]
             [tic-tac-toe.game :refer :all]
             [tic-tac-toe.ai :refer :all]
@@ -20,9 +20,9 @@
 
 (defrecord DummyInput []
   InputProtocol
-  (get-move [input board]  )
+  (get-move [input board] (user-input-move board))
   (get-board-size [input]  3)
-  (get-game-type [input games] :computer-vs-human ))
+  (get-game-type [input games] 1))
 
 (defmethod create-game :dummy-game [game-type input board ]
     [game-type input  board ])
@@ -45,29 +45,12 @@
     (test " returns a starting game state and players within a map")
       (is (= [player-1 player-2] (last test-game)))))
 
-; (deftest game-iterattion-test
-;    (let [starting-state { :board [["_" "o" "o" ]["x" "_" "_" ]["_" "_" "_" ]] :ai-marker "o"  :player-marker "x"}
-;          closing-state [["o" "o" "o" ]["x" "_" "x" ]["_" "_" "_" ]]
-;          terminal (->DummyDisplay)
-;           human (->HumanPlayer "x" (->ConsoleInput))
-;           ai (->AiPlayer "o")]
-;      (test "this test confirms the game follows the correct game iteration sequences")
-;      (with-in-str "6" (game-runner starting-state terminal human ai))
-;      (is (= (:states @latest-displayed-state)  closing-state))))
-
-; (deftest game-iterattion-test
-;   (let [display(->DummyDisplay)
-;         input(->ConsoleInput)
-;         board (create-empty-board 3)
-;         game (create-game :computer-vs-human input board)]
-;   (test "this test confirms the game follows the correct game iteration sequences")
-;    (with-in-str  (with-in-str "3\n3\n9\n8\n" (game-runner game display )))))
 
 (deftest game-intializer-test
    (let [display (->DummyDisplay)
          input (->DummyInput)]
      (test "game initialize  with correct arguments")
-     (with-out-str (with-in-str "2\n3\n3\n9\n8\n"(game-intializer display input)))
-     (is (= latest-displayed-state " " ))))
+     (with-out-str (with-in-str "3\n3\n9\n8\n"(game-intializer display input)))
+     (is (= (@latest-displayed-state :winner) "o"))))
 
 
