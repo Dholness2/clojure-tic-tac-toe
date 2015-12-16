f6c93fdacd322f695f49bb1f3130afd69265512a
Donovan Holness <dholness@8thlight.com>
2015-12-16 10:23:38 -0800

    made game intializer more verbose

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,7 +1,7 @@
 (ns tic-tac-toe.core
   (:gen-class)
   (require [tic-tac-toe.board :refer [create-empty-board]]
-            [clojure.core.matrix :refer [rotate]]
+           [clojure.core.matrix :refer [rotate]]
            [tic-tac-toe.game :refer [winner?]]
            [tic-tac-toe.ai :refer [->AiPlayer ]]
            [tic-tac-toe.display.terminal :refer [->TerminalDisplay print-winner]]
@@ -17,14 +17,14 @@
 
 (defn game-runner [game display]
   (let [game-state (first game)
-        players    (last game )]
+        players (last game)]
     (display-state display (:board game-state))
     (if-not (winner? (:board game-state))
       (let [current-state (next-move (first players) game-state)]
         (display-state display (:board current-state))
         (if-not (winner? (:board current-state))
           (game-runner [current-state (rotate players 0 1)] display)
-          (display-winner display  (:board current-state)))))))
+          (display-winner display (:board current-state)))))))
 
 (defmulti create-game (fn [game-type input board ] game-type))
 
@@ -40,15 +40,15 @@
         game-state {:board board :ai-marker marker-one :player-marker marker-two}]
     [game-state [player-1 player-2]]))
 
-(defn game-intializer [display input]
-  (let [play-type (games (get-game-type input games))
-        board-dimension (get-board-size input)
-        board (create-empty-board board-dimension)
-        game (create-game play-type input  board)]
+(defn game-intializer [display prompter]
+  (let [game-type-selection (games (get-game-type prompter games))
+        board-size-selection (get-board-size prompter)
+        empty-board (create-empty-board board-size-selection)
+        game (create-game game-type-selection prompter empty-board)]
         (game-runner game display)))
 
 (defn -main []
-  (let [terminal (->TerminalDisplay)
-        input (->ConsoleInput)]
-    (game-intializer terminal input)))
+  (let [display (->TerminalDisplay)
+        prompter (->ConsoleInput)]
+    (game-intializer display prompter)))
 
